10a2b8c3a2be94ea8c3585df3925882156e06336
sskiragu <skiragu@gmail.com>
2023-01-19 16:01:54 +0300

    added dashboard

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .top-nav{
+            background-color: darkorange;
+            padding: 14px 28px;
+        }
+        </style>
+</head>
+<body>
+    <div class="top-nav">
+        <a href="view.php">View</a>
+    </div>
+</body>
+</html>
