40c2dfc467d52d2f77f8772bbd0e54f325f8f008
sskiragu <skiragu@gmail.com>
2023-01-23 16:00:06 +0300

    Refactored view

--- view.php
+++ view.php
@@ -1,13 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
 <?php
-
 include 'dbConnect.php';
 
 $sql = "SELECT * FROM details";
 
 $result = $db_connect->query($sql);
 
+echo "
+    <table class='table'>
+        <thead>
+            <tr>
+                <th>Email</th>
+                <th>Username</th>
+                <th colspan='2'>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+";
 while($row = $result->fetch_assoc()){
-    echo $row['email'] ." ". $row['username'] ."<br />";
+    echo "<tr>
+        <td>".$row['email']."</td>
+        <td>".$row['username']."</td>
+        <td><a href='edit.php?id=$row[id]'><svg class='text-success' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+        <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
+        <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
+            </svg></a></td>
+        <td><a href='delete.php?id=$row[id]'><svg class='text-danger' xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-archive-fill' viewBox='0 0 16 16'>
+        <path d='M12.643 15C13.979 15 15 13.845 15 12.5V5H1v7.5C1 13.845 2.021 15 3.357 15h9.286zM5.5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1zM.8 1a.8.8 0 0 0-.8.8V3a.8.8 0 0 0 .8.8h14.4A.8.8 0 0 0 16 3V1.8a.8.8 0 0 0-.8-.8H.8z'/>
+            </svg></a></td>
+    </tr>";
 }
+
+echo "</tbody></table>"
 
 ?>
